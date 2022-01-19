17e627e3ac8ea416e4fe1308810f80bcacb69da8
Christer Kaitila <breakdance@mcfunkypants.com>
2022-01-19 12:13:31 -0800

    slight adjustment to spooky eyes' y coordinates

--- js/spookyEyes.js
+++ js/spookyEyes.js
@@ -8,7 +8,7 @@ function spookyEyes(x,y,eysOffset=3) {
     const centerPlayerY = 15;
     let lookAngle = Math.atan2(p1.y-y+centerPlayerY,p1.x-x+centerPlayerX);
     x += Math.round(Math.cos(lookAngle)*moveSizeX);
-    y += Math.round(Math.sin(lookAngle)*moveSizeY);
+    y += Math.ceil(Math.sin(lookAngle)*moveSizeY);
     canvasContext.drawImage(spookyEye,x+eysOffset,y);
     canvasContext.drawImage(spookyEye,x-eysOffset,y);
 }
