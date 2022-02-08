5c7499069357914d9995b47cbb923c31da75eaad
Christer Kaitila <breakdance@mcfunkypants.com>
2022-02-08 12:30:36 -0800

    octoGolem improved

--- js/octoGolem.js
+++ js/octoGolem.js
@@ -24,24 +24,29 @@ class octoGolem extends enemy {
         canvasContext.drawImage(this.myBitmap,this.x,this.y-31);
         
         // draw the six tentacles
-        const LINKS = 8;
+        const LINKS = 12;
         const LINKSPACING = 8;
-        const ARMS = 3;
-        const ARMSPACING = 16;
-        const WOBBLESIZE = 16;
-        const WOBBLESPD = .005;
-        let linkx, linky, linkwobble, armdir;
+        const ARMS = 4; // per side
+        const ARMSPACING = 10; // vertically
+        const WOBBLESIZE = 50;
+        const WOBBLESPD = .0025;
+        const SHOULDERWIDTH = 60;
+        const ARMOFFSETX = -15;
+        const ARMOFFSETY = -30;
+        let linkx, linky, linkwobble, armdir, linkpic;
         for (let sidenum=0; sidenum<2; sidenum++) {
             for (let armnum=0; armnum<ARMS; armnum++) {
                 for (let linknum=0; linknum<LINKS; linknum++) {
                     armdir = sidenum?1:-1;
-                    linkx = this.x + armdir*(linknum*LINKSPACING);
-                    if (sidenum==1) linkx += 30;
-                    linkwobble = Math.sin((performance.now()+linkx*10+armnum*15)*(WOBBLESPD+armnum/500))*WOBBLESIZE;
+                    linkx = this.x+ARMOFFSETX+armdir*(linknum*LINKSPACING);
+                    if (sidenum==1) linkx += SHOULDERWIDTH;
+                    linkwobble = Math.sin((performance.now()+linkx*10+armnum*15)*(WOBBLESPD+armnum/1500))*WOBBLESIZE;
                     linkwobble *= linknum/LINKS; // smaller movement near shoulders
-                    linky = this.y+10+linkwobble;
+                    linky = this.y+ARMOFFSETY+linkwobble;
                     linky += armnum*ARMSPACING;
-                    canvasContext.drawImage(linknum%2?chain1Pic:chain2Pic,linkx,linky);
+                    //linkpic = linknum%2?chain1Pic:chain2Pic; // alternate images! like a real chain
+                    linkpic = chain1Pic; // all the same image
+                    canvasContext.drawImage(linkpic,linkx,linky);
                 }
             }
         }
